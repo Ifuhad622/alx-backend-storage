7f8d018902c4b94d1a3317f75f0c6c2e0cc9205e
Ifuhad622 <ifuhad007@gmail>
2024-07-16 20:57:29 +0000

    0x01-NoSQL

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,16 @@
-// Task 13 - Advanced - Lists all documents with spec. attr. in collection.
-db.school.find({name: /Holberton.*/}).forEach(printjson);
+#!/usr/bin/env python3
+"""
+Module: Task 11.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns the list of school having a specific topic."""
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
